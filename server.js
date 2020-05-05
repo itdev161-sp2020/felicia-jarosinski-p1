3536de87bddeaefe12a3ee85e85fa34a3d9e6e12
Felicia J <jarosinf@gmatc.matc.edu>
2020-05-05 17:43:33 -0500

    connecting to MongoDB

--- server.js
+++ server.js
@@ -1,7 +1,14 @@
 import express from 'express';
+import connectDatabase from './config/db';
 //initialize express application
 const app = express();
+
+//connect database
+connectDatabase();
+
 //API endpoints
-app.get('/', (req, res) => res.send('http get request sent to root api endpoint'));
+app.get('/', (req, res) => 
+    res.send('http get request sent to root api endpoint')
+);
 //Connection listener
 app.listen(3000, () => console.log('Express server running on port 3000'));
