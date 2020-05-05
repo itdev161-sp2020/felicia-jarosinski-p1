956352793a320839ae5c2f9a2e0369274b39e6da
Felicia J <jarosinf@gmatc.matc.edu>
2020-05-05 15:57:39 -0500

    updated scripts for Express connection

---
+++ server.js
@@ -0,0 +1,7 @@
+import express from 'express';
+//initialize express application
+const app = express();
+//API endpoints
+app.get('/', (req, res) => res.send('http get request sent to root api endpoint'));
+//Connection listener
+app.listen(3000, () => console.log('Express server running on port 3000'));
