d6cabd20bf0d3991df52f1bf4a49dfa417e35b5a
Felicia J <jarosinf@gmatc.matc.edu>
2020-05-06 15:21:22 -0500

    Completed API connection for front and backend components

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,14 +1,31 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import axios from 'axios';
 
 class App extends React.Component {
-  render () {
+  state = {
+    data: null
+  }
+
+  componentDidMount() {
+    axios.get('http://localhost:5000')
+    .then((response) => {
+      this.setState({
+        data: response.data
+      })
+    })
+    .catch((error) => {
+      console.error('Error fetching data: ${error}');
+    })
+  }
+
+  render() {
     return (
       <div className="App">
         <header className="App-header">
           GoodThings
         </header>
+        {this.state.data}
       </div>
     );
   }
--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 import express from "express";
 import connectDatabase from "./config/db";
 import { check, validationResult } from "express-validator";
+import cors from 'cors';
 
 //initialize express application
 const app = express();
@@ -10,6 +11,11 @@ connectDatabase();
 
 //Configure Middleware
 app.use(express.json({ extended: false }));
+app.use(
+    cors({
+        origin: 'http://localhost:3000'
+    })
+);
 
 //API endpoints
 /**
@@ -46,4 +52,4 @@ app.post(
 
 //Connection listener
 const port = 5000;
-app.listen(port, () => console.log('Express server running on port ${port}')));
+app.listen(port, () => console.log('Express server running on port ${port}'));
