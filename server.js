2494221be07c44d162c1ac1c47a4cd95d38c4e9c
Felicia J <jarosinf@gmatc.matc.edu>
2020-05-22 22:42:43 -0500

    created post endpoints

--- server.js
+++ server.js
@@ -1,11 +1,12 @@
 import express from 'express';
 import connectDatabase from './config/db';
-import { check, validationResult } from 'express-validator';
+import { check, validationResult, sanitizeBody } from 'express-validator';
 import cors from 'cors';
 import bcrypt from 'bcryptjs';
 import jwt from 'jsonwebtoken';
 import config from 'config';
 import User from './models/User';
+import Post from './models/Post';
 import auth from './middleware/auth';
 
 //initialize express application
@@ -137,7 +138,49 @@ app.post(
     }
   }
 );
-
+//Post endpoints
+/**
+ * 
+ * @route POST api/posts
+ * @desc Create post
+ */
+app.post(
+  '/api/posts',
+  [
+    auth,
+    [check('title', 'Title text is required')
+    .not()
+    .isEmpty(),
+  check('body', 'Body text is required')
+    .not()
+    .isEmpty()
+  ]
+  ],
+  async(req, res)=> {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()){
+      res.status(400).json({errors: errors.array() });
+    }else {
+      const {title, body} = req.body;
+      try{
+        //get the user who created the post
+        const user = await User.findById(req.user.id);
+        //create a new post
+        const post = new Post({
+          user: user.id,
+          title: title,
+          body: body
+        });
+        //save to the db and return 
+        await post.save();
+        res.json(post);
+      }catch(error) {
+        console.error(error);
+        res.status(500).send('Server error');
+      }
+    }
+  }
+);
 const returnToken = (user, res) => {
   const payload ={
     user: {
